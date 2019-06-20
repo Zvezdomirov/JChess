bf98b0cf4a5995fd9c2ab48fbf3c8b223143e886
Zahari Zvezdomirov <z.zvezdomirov@gmail.com>
2019-06-20 12:05:04 +0300

    Update DriverClass.java

--- src/com/chess/engine/game/DriverClass.java
+++ src/com/chess/engine/game/DriverClass.java
@@ -9,9 +9,9 @@ import java.io.OutputStreamWriter;
 
 public class DriverClass {
     public static void main(String[] args) throws IOException {
-        Player pesho = new Player("Pesho", "chushki", Alliance.WHITE);
-        Player gosho = new Player("Gosho", "domati", Alliance.BLACK);
-        Game chessGame = new Game(pesho, gosho);
+        Player john= new Player("John", "pass123", Alliance.WHITE);
+        Player peter = new Player("Peter", "pass456", Alliance.BLACK);
+        Game chessGame = new Game(john, peter);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         chessGame.getGameBoard().displayBoard(bw);
         bw.flush();
